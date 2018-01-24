7ef6f01a795d2f3447319e293453039990591922
Pierre Neter <pierreneter@gmail.com>
2018-01-25 05:13:25 +0700

    Add get method, so can get info at electron website url

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {post} = require('got')
+const got = require('got')
 const assert = require('assert')
 const path = require('path')
 
@@ -8,14 +8,18 @@ async function getCrowdinFileIds (project, crowdinKey) {
 
   assert(project, '`project` must be the first argument')
 
+  let method = 'post'
+
   if (project === 'electron') {
     url = 'https://electronjs.org/crowdin/info'
+    method = 'get' // Fix: HTTPError: Response code 405 (Method Not Allowed)
+                   // https://github.com/electron/electronjs.org/blob/f67079064ea071bee1ad46052d7940591e229a2c/routes/languages/proxy.js#L17
   } else {
     assert(crowdinKey, '`crowdinKey` must be the second argument or process.env.CROWDIN_KEY')
     url = `https://api.crowdin.com/api/project/${project}/info?key=${crowdinKey}&json`
   }
 
-  const res = await post(url, {json: true})
+  const res = await got[method](url, {json: true})
   return resolveFile({}, res.body.files)
 }
 
@@ -25,7 +29,7 @@ function resolveFile (acc, files, pathPrefix = []) {
   files.forEach(file => {
     switch (file.node_type) {
       case 'file':
-        const fullpath = pathPrefix.concat(file.name).join(path.sep)
+        const fullpath = pathPrefix.concat(file.name).join(path.sep).replace(/\\/g, '/')
         acc[fullpath] = file.id
         break
       case 'directory':
